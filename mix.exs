5aed1f5ca2babb4af333e1f519b58f5f2795922d
Jan Fornoff <jan.fornoff@grandcentrix.net>
2018-04-16 09:48:04 +0200

    Release 0.7.0

--- mix.exs
+++ mix.exs
@@ -2,19 +2,20 @@ defmodule Vex.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :vex,
-      version: "0.6.0",
+    [
+      app: :vex,
+      version: "0.7.0",
       elixir: "~> 1.2",
       deps: deps(),
-      consolidate_protocols: Mix.env != :test,
+      consolidate_protocols: Mix.env() != :test,
       package: package(),
 
       # Docs
       name: "Vex",
       source_url: "https://github.com/CargoSense/vex",
       homepage_url: "https://github.com/CargoSense/vex",
-      docs: [main: "readme",
-             extras: ["README.md"]]]
+      docs: [main: "readme", extras: ["README.md"]]
+    ]
   end
 
   # Configuration for the OTP application
@@ -27,11 +28,12 @@ defmodule Vex.Mixfile do
   end
 
   defp package do
-    [contributors: ["Bruce Williams", "Ben Wilson", "John Hyland"],
-     maintainers: ["Bruce Williams", "Ben Wilson", "John Hyland"],
-     licenses: ["MIT License"],
-     description: "An extensible data validation library for Elixir",
-     links: %{github: "https://github.com/CargoSense/vex"}]
+    [
+      contributors: ["Bruce Williams", "Ben Wilson", "John Hyland"],
+      maintainers: ["Bruce Williams", "Ben Wilson", "John Hyland"],
+      licenses: ["MIT License"],
+      description: "An extensible data validation library for Elixir",
+      links: %{github: "https://github.com/CargoSense/vex"}
+    ]
   end
-
 end
