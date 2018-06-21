dfd399724f7950d91fe6d894bb5a02e3d27bfa24
Jan Fornoff <jan.fornoff@grandcentrix.net>
2018-06-21 13:07:06 +0200

    Bump version for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vex.Mixfile do
   def project do
     [
       app: :vex,
-      version: "0.7.0",
+      version: "0.8.0",
       elixir: "~> 1.2",
       deps: deps(),
       consolidate_protocols: Mix.env() != :test,
