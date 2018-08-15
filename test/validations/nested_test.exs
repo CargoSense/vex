3117af59ea34a9c1c0f6045bfe35c03b48eda5d5
Anastasiya Dyachenko <apelsinka223@gmail.com>
2018-08-15 23:14:34 +0300

    without descibe

--- test/validations/nested_test.exs
+++ test/validations/nested_test.exs
@@ -8,65 +8,61 @@ end
 defmodule NestedTest do
   use ExUnit.Case
 
-  describe "keyword list" do
-    test "nested" do
-      assert Vex.valid?([author: [name: "Foo"]], %{[:author, :name]  => [presence: true]})
-      assert Vex.valid?([author: %{name: "Foo"}], %{[:author, :name]  => [presence: true]})
+  test "keyword list, nested" do
+    assert Vex.valid?([author: [name: "Foo"]], %{[:author, :name]  => [presence: true]})
+    assert Vex.valid?([author: %{name: "Foo"}], %{[:author, :name]  => [presence: true]})
 
-      nested_errors_list = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors_list == Vex.errors([author: [name: ""]], %{[:author, :name]  => [presence: true]})
+    nested_errors_list = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors_list == Vex.errors([author: [name: ""]], %{[:author, :name]  => [presence: true]})
 
-      nested_errors_mixed = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors_mixed == Vex.errors([author: %{name: ""}], %{[:author, :name]  => [presence: true]})
-    end
-
-    test "nested with _vex" do
-      assert Vex.valid?([author: [name: "Foo"], _vex: %{[:author, :name]  => [presence: true]}])
-      assert Vex.valid?([author: %{name: "Foo"}, _vex: %{[:author, :name]  => [presence: true]}])
-
-      nested_errors_list = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors_list == Vex.errors([author: [name: ""], _vex: %{[:author, :name]  => [presence: true]}])
-
-      nested_errors_mixed = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors_mixed == Vex.errors([author: %{name: ""}, _vex: %{[:author, :name]  => [presence: true]}])
-    end
-
-    test "nested in Record" do
-      assert Vex.valid?(%NestedTestRecord{author: [name: "Foo"]})
-
-      nested_errors = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors == Vex.errors(%NestedTestRecord{author: [name: ""]})
-    end
+    nested_errors_mixed = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors_mixed == Vex.errors([author: %{name: ""}], %{[:author, :name]  => [presence: true]})
   end
 
-  describe "map" do
-    test "nested " do
-      assert Vex.valid?(%{author: %{name: "Foo"}}, %{[:author, :name]  => [presence: true]})
-      assert Vex.valid?(%{author: [name: "Foo"]}, %{[:author, :name]  => [presence: true]})
+  test "keyword list, nested with _vex" do
+    assert Vex.valid?([author: [name: "Foo"], _vex: %{[:author, :name]  => [presence: true]}])
+    assert Vex.valid?([author: %{name: "Foo"}, _vex: %{[:author, :name]  => [presence: true]}])
 
-      nested_errors_map = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors_map == Vex.errors(%{author: %{name: ""}}, %{[:author, :name]  => [presence: true]})
+    nested_errors_list = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors_list == Vex.errors([author: [name: ""], _vex: %{[:author, :name]  => [presence: true]}])
 
-      nested_errors_mixed = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors_mixed == Vex.errors(%{author: [name: ""]}, %{[:author, :name]  => [presence: true]})
-    end
+    nested_errors_mixed = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors_mixed == Vex.errors([author: %{name: ""}, _vex: %{[:author, :name]  => [presence: true]}])
+  end
 
-    test "nested with _vex" do
-      assert Vex.valid?(%{author: %{name: "Foo"}, _vex: %{[:author, :name]  => [presence: true]}})
-      assert Vex.valid?(%{author: [name: "Foo"], _vex: %{[:author, :name]  => [presence: true]}})
+  test "keyword list, nested in Record" do
+    assert Vex.valid?(%NestedTestRecord{author: [name: "Foo"]})
 
-      nested_errors_map = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors_map == Vex.errors(%{author: %{name: ""}, _vex: %{[:author, :name]  => [presence: true]}})
+    nested_errors = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors == Vex.errors(%NestedTestRecord{author: [name: ""]})
+  end
 
-      nested_errors_mixed = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors_mixed == Vex.errors(%{author: [name: ""], _vex: %{[:author, :name]  => [presence: true]}})
-    end
+  test "map, nested" do
+    assert Vex.valid?(%{author: %{name: "Foo"}}, %{[:author, :name]  => [presence: true]})
+    assert Vex.valid?(%{author: [name: "Foo"]}, %{[:author, :name]  => [presence: true]})
 
-    test "nested in Record" do
-      assert Vex.valid?(%NestedTestRecord{author: %{name: "Foo"}})
+    nested_errors_map = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors_map == Vex.errors(%{author: %{name: ""}}, %{[:author, :name]  => [presence: true]})
 
-      nested_errors = [{:error, [:author, :name], :presence, "must be present"}]
-      assert nested_errors == Vex.errors(%NestedTestRecord{author: %{name: ""}})
-    end
+    nested_errors_mixed = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors_mixed == Vex.errors(%{author: [name: ""]}, %{[:author, :name]  => [presence: true]})
+  end
+
+  test "map, nested with _vex" do
+    assert Vex.valid?(%{author: %{name: "Foo"}, _vex: %{[:author, :name]  => [presence: true]}})
+    assert Vex.valid?(%{author: [name: "Foo"], _vex: %{[:author, :name]  => [presence: true]}})
+
+    nested_errors_map = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors_map == Vex.errors(%{author: %{name: ""}, _vex: %{[:author, :name]  => [presence: true]}})
+
+    nested_errors_mixed = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors_mixed == Vex.errors(%{author: [name: ""], _vex: %{[:author, :name]  => [presence: true]}})
+  end
+
+  test "map, nested in Record" do
+    assert Vex.valid?(%NestedTestRecord{author: %{name: "Foo"}})
+
+    nested_errors = [{:error, [:author, :name], :presence, "must be present"}]
+    assert nested_errors == Vex.errors(%NestedTestRecord{author: %{name: ""}})
   end
 end
